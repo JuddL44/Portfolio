4a8295e95e45b8cfdd86f87a954bc63861ba720a
JuddL44 <businessjudd@gmail.com>
2026-04-17 14:56:38 -0500

    Create basic site layout

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('Frontend');
+  protected readonly title = signal('Judd Lasater');
+
+  header: string = 'Judd Lasater';
+  description: string = 'Full-Stack Developer | .NET · Angular · Azure';
+  bgColor: string = `linear-gradient(90deg, hsla(0, 67%, 8%, 1) 0%, hsla(26, 92%, 10%, 1) 48%, hsla(323, 76%, 8%, 1) 100%)`;
+
+  @HostListener('window:scroll')
+  onScroll() {
+    const scrollTop = window.scrollY;
+    const docHeight = document.body.scrollHeight - window.innerHeight;
+    const progress = scrollTop / docHeight;
+    if (progress < 0.33) {
+      this.bgColor = 'rgb(15, 5, 8)';
+    } else if (progress < 0.66) {
+      this.bgColor = 'rgb(25, 10, 15)';
+    } else {
+      this.bgColor = 'rgb(20, 15, 5)';
+    }
+  }
 }
